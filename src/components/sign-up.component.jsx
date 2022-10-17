10eceed3236a24f6d232b8c3d6413e766c782b02
Ruihua Ryan Su <rh.ryan.su@gmail.com>
2022-10-17 20:56:46 +1100

    Migration from NPM to YARN

--- src/components/sign-up.component.jsx
+++ src/components/sign-up.component.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { DebounceInput } from "react-debounce-input";
 import { createAuthUserWithEmailAndPassword } from "../utils/firebase/firebase.utils";
 import { ReactComponent as Tick } from "../assets/icons8-done.svg";
@@ -11,26 +11,46 @@ function SignUp() {
     confirmPassword: "",
   };
 
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [btnStatus, setBtnStatus] = useState("Sign up");
 
   console.log(formFields);
 
   const handleSubmit = async (event) => {
-
     event.preventDefault();
-    
+
     if (event.target.password.value !== event.target.confirmPassword.value) {
-      alert("Passwords don't match");
+      passwordRef.current.focus();
       return;
     } else {
-      if (1 === 0) await createAuthUserWithEmailAndPassword(email, password);
+      try {
+        setBtnStatus("Processing...");
+        const response = await createAuthUserWithEmailAndPassword(
+          email,
+          password
+        );
+        setBtnStatus("Success");
+        await timeout(2000);
+        resetFormFields();
+        console.log(response);
+      } catch (error) {
+        if (error.code === "auth/email-already-in-use") {
+          setFormFields({ ...formFields, email: "" });
+          emailRef.current.focus();
+        }
+        setBtnStatus("Oops, try again");
+        await timeout(2000);
+        console.error(error);
+      }
 
-      setBtnStatus("Processing...");
-      await timeout(3000);
-      setBtnStatus("Success");
-      await timeout(2000);
-      setBtnStatus("Sign Up");
+      setBtnStatus("Sign up");
     }
   };
 
@@ -43,19 +63,17 @@ function SignUp() {
     setFormFields({ ...formFields, [name]: value });
   };
 
-  const [btnStatus, setBtnStatus] = useState("Sign Up");
-
   return (
     <div className="flex flex-col justify-center">
       <h1 className="font-semibold text-blue-500">
         Sign Up Here with Email and Password
       </h1>
-      <form className="mt-4 flex flex-col items-start" onSubmit={handleSubmit}>
+      <form className="mt-4 flex flex-col items-star pt-5" onSubmit={handleSubmit}>
         <label>Display Name</label>
         <DebounceInput
           required
           type="text"
-          className="w-full p-1 my-2 border rounded"
+          className="w-full p-1 my-2 border rounded box-border px-2"
           name="displayName"
           value={displayName}
           onChange={handleOnChange}
@@ -65,8 +83,9 @@ function SignUp() {
         <label>Email</label>
         <DebounceInput
           required
+          inputRef={emailRef}
           type="email"
-          className="w-full p-1 my-2 border rounded"
+          className="w-full p-1 my-2 border rounded box-border px-2"
           name="email"
           value={email}
           onChange={handleOnChange}
@@ -77,7 +96,7 @@ function SignUp() {
         <DebounceInput
           required
           type="password"
-          className="w-full p-1 my-2 border rounded"
+          className="w-full p-1 my-2 border rounded box-border px-2"
           name="password"
           value={password}
           onChange={handleOnChange}
@@ -89,8 +108,9 @@ function SignUp() {
         <label>Confirm Password</label>
         <DebounceInput
           required
+          inputRef={passwordRef}
           type="password"
-          className="w-full p-1 my-2 border rounded"
+          className="w-full p-1 my-2 border rounded box-border px-2"
           name="confirmPassword"
           value={confirmPassword}
           onChange={handleOnChange}
@@ -100,31 +120,31 @@ function SignUp() {
         />
         <button
           type="submit"
-          className="inline-flex items-center justify-center w-full p-1 mt-2 text-white border rounded bg-blue-500 border-blue-500 hover:bg-white hover:text-blue-500 transition ease-in-out duration-500"
+          className="inline-flex items-center justify-center w-full p-1 mt-5 text-white border rounded bg-blue-500 border-blue-500 hover:bg-white hover:text-blue-500 transition ease-in-out duration-500"
         >
           {btnStatus === "Processing..." ? (
             <svg
-              class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-inherit"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
             >
               <circle
-                class="opacity-25"
+                className="opacity-25"
                 cx="12"
                 cy="12"
                 r="10"
                 stroke="currentColor"
-                stroke-width="4"
+                strokeWidth="4"
               ></circle>
               <path
-                class="opacity-75"
+                className="opacity-75"
                 fill="currentColor"
                 d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
               ></path>
             </svg>
           ) : null}
-          {btnStatus === "Success" ? <Tick className="invert mr-2" /> : null}
+          {btnStatus === "Success" ? <Tick className="mr-2" /> : null}
           {btnStatus}
         </button>
       </form>
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,4 +1,3 @@
-import { async } from "@firebase/util";
 import React from "react";
 import {
   signInWithGoogle,
@@ -7,8 +6,19 @@ import {
 } from "../../utils/firebase/firebase.utils";
 import { SocialIcon } from "react-social-icons";
 import SignUp from "../../components/sign-up.component";
+import { DebounceInput } from "react-debounce-input";
+import { useState } from "react";
 
 function SignIn() {
+  const defaultFormFields = {
+    email: "",
+    password: "",
+  };
+
+  const [formFields, setFormFields] = useState(defaultFormFields);
+
+  const { email, password } = formFields;
+
   const logGoogleUser = async () => {
     const response = await signInWithGoogle();
     const userDocRef = await createUserDocumentFromAuth(response.user);
@@ -19,24 +29,67 @@ function SignIn() {
     const userDocRef = await createUserDocumentFromAuth(response.user);
   };
 
+  const handleOnChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="flex justify-evenly">
       <div className="flex flex-col my-40 px-8 items-center">
+        <div className="flex flex-col">
+          <p className="font-semibold text-blue-500">Sign In Here with Email and Password</p>
+          <form onSubmit={handleSubmit} className="flex flex-col pt-5">
+            <label>Email</label>
+            <DebounceInput
+              required
+              type="email"
+              className="p-1 my-2 border rounded box-border px-2"
+              name="email"
+              value={email}
+              onChange={handleOnChange}
+              debounceTimeout={500}
+            />
+            <label>Password</label>
+            <DebounceInput
+              required
+              type="password"
+              className="p-1 my-2 border rounded box-border px-2"
+              name="password"
+              value={password}
+              onChange={handleOnChange}
+              debounceTimeout={500}
+            />
+            <button type="submit" className="inline-flex items-center justify-center w-full p-1 mt-5 text-white border rounded bg-blue-500 border-blue-500 hover:bg-white hover:text-blue-500 transition ease-in-out duration-500">
+              Login
+            </button>
+          </form>
+        </div>
+        <div className="w-full text-center border-b border-solid leading-[0.1rem] mt-12 mb-10">
+          <span className="bg-[#fff] px-2">or sign in with</span>
+        </div>
         <button
+          type="button"
           onClick={logGoogleUser}
-          className="w-full my-1 p-1 font-semibold border rounded border-hidden hover:bg-gray-300 transition ease-in-out duration-500"
+          className="w-full p-1 font-semibold border rounded border-hidden hover:bg-gray-300 transition ease-in-out duration-500"
         >
-          <SocialIcon className="mx-2" network="google" />
+          <SocialIcon className="mr-5" network="google" />
           Sign in with Google
         </button>
         <button
+          type="button"
           onClick={logGithubUser}
           className="w-full my-1 p-1 font-semibold border rounded border-hidden hover:bg-gray-300 transition ease-in-out duration-500"
         >
-          <SocialIcon className="mx-2" network="github" />
+          <SocialIcon className="mr-5" network="github" />
           Sign in with GitHub
         </button>
       </div>
+      <div className="border-l border-slate-400/50"></div>
       <div className="flex">
         <SignUp />
       </div>
